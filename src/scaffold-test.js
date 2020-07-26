67e23728bc2e32ec69f9ad5e2e83f4740ff08f4c
Matt Travi <programmer@travi.org>
2020-07-25 22:26:52 -0500

    feat(env): added template for example file

---
+++ src/scaffold-test.js
@@ -0,0 +1,29 @@
+import {promises as fs} from 'fs';
+import {resolve} from 'path';
+import sinon from 'sinon';
+import {assert} from 'chai';
+import any from '@travi/any';
+import scaffold from './scaffold';
+
+suite('scaffold', () => {
+  let sandbox;
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(fs, 'copyFile');
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that probot is initialized', async () => {
+    const projectRoot = any.string();
+
+    await scaffold({projectRoot});
+
+    assert.calledWith(
+      fs.copyFile, resolve(__dirname, '..', 'templates', '.env.example'),
+      `${projectRoot}/.env.example`
+    );
+  });
+});
